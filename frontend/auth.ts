c0955550885b4f1a04d0d18a42bf32709a1a88ff
Thomas Marconi <thomas.marconi@outlook.com>
2026-01-04 03:59:10 -0500

    added prisma for the auth models

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 });
